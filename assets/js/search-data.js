0e9aee08bcadbd9d3b5784d6858c2b28d498722d
imczxx <2293210934@qq.com>
2025-01-07 01:36:34 +0000

    Deploying to gh-pages from @ imczxx/al-folio@440804ae86720be1e4a1eecae9b5cb30d2c502cc 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,182 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/al-folio/";
+    },
+  },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/news/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/people/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/projects/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/publications/";
+          },
+        },{id: "nav-funding",
+          title: "funding",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/funding/";
+          },
+        },{id: "nav-join-us",
+          title: "join us",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/join_us/";
+          },
+        },{id: "dropdown-publications",
+              title: "publications",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-projects",
+              title: "projects",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/al-folio/blog/";
+              },
+            },{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/al-folio/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:A simple inline announcement with...',
+          description: "",
+          section: "News",},{id: "people-example1",
+          title: 'Example1',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example1/";
+            },},{id: "people-example2",
+          title: 'Example2',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example2/";
+            },},{id: "people-example3",
+          title: 'Example3',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example3/";
+            },},{id: "people-example4",
+          title: 'Example4',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example4/";
+            },},{id: "people-example5",
+          title: 'Example5',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example5/";
+            },},{id: "people-example6",
+          title: 'Example6',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/al-folio/people/example6/";
+            },},{id: "projects-minivla-a-better-vla-with-a-smaller-footprint",
+          title: 'MiniVLA: A Better VLA with a Smaller Footprint',
+          description: "Reducing OpenVLA&#39;s parameters 7x, and improving the input and output representation space.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/9_project/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/al-folio/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
